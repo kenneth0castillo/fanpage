9bcb02318a4d5df72261b1409539a23f2c7cc18d
Kenneth Castillo <kennethc6970@uamusicandart.org>
2018-11-06 22:22:05 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 // JavaScript File
 /*global $*/
 
-$('body').click(function(){
+// $('body').click(function(){
     
-});
+// });
+// var slideIndex = 0;
+// carousel();
+
+// function carousel() {
+//     var i;
+//     var x = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < x.length; i++) {
+//       x[i].style.display = "none"; 
+//     }
+//     slideIndex++;
+//     if (slideIndex > x.length) {slideIndex = 1} 
+//     x[slideIndex-1].style.display = "block"; 
+//     setTimeout(carousel, 5000);
+// }
+
 
